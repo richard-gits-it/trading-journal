271829e3e403c9ffb7f339788fee261b03165ffc
richard-gits-it <113040368+richard-gits-it@users.noreply.github.com>
2025-12-14 01:01:54 -0700

    Update init-db.js

--- api/init-db.js
+++ api/init-db.js
@@ -1,7 +1,11 @@
 import { sql } from '@vercel/postgres';
 
+export const config = {
+  runtime: 'nodejs',
+};
+
 export default async function handler(req, res) {
-  if (req.method !== 'POST') {
+  if (req.method !== 'GET') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
 
